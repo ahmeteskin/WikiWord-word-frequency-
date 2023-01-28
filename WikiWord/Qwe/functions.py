eea5aa4a918768b0838c69a61ca8a54cd9f475d3
ahmeteskin <123831966+ahmeteskin@users.noreply.github.com>
2023-01-28 21:24:38 +0300

    Update functions.py

--- WikiWord/Qwe/functions.py
+++ WikiWord/Qwe/functions.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from WikiWord.Qwe import values
 from WikiWord.Qwe import sqldb
-#from WikiWord.Qwe import functions
 import string
 import re
 
@@ -11,8 +10,7 @@ class WikiWor(webdriver.Chrome):
     def __init__(self, switch = True):
         super(WikiWor, self).__init__()
         self.sw = switch
-        #self.newdic = WikiWor().calculate()
-
+        
     def onoff(self):
         return self.sw
 
@@ -32,10 +30,6 @@ class WikiWor(webdriver.Chrome):
         self.implicitly_wait(10)
         gett = self.find_elements(By.TAG_NAME, "p")
         self.gett = gett
-
-        #for i in get:
-        #    print(i.text)
-
         return self.gett
 
     def calculate(self):
@@ -43,14 +37,8 @@ class WikiWor(webdriver.Chrome):
         newdic = dict()
         count = 0
         for line in WikiWor.get_words(self):
-            #lin = line.text.rstrip()
             lin = line.text.lower()
 
-            ##for i in string.punctuation:
-                ##lin = lin.replace(i, '')
-                #lin = lin.replace(" ", "")
-                #c = re.sub("[0-9]+", "", line)
-                #t = lin.replace(i, '')
             a = lin.split()
             for p in range(len(a)):
                     if "[" in a[p]:
@@ -67,13 +55,8 @@ class WikiWor(webdriver.Chrome):
         for q, w in dic.items():
             newdic[q] = "{} %".format(float(w / count) * 100)
 
-        #self.newdic = newdic
-        #print(newdic)
         t = open("tex.text", "w", encoding="utf-8")
         t.write(f"{newdic}")
-        ##return newdic
-
-
 
     def db(self):
         y = sqldb.SqlDb()
